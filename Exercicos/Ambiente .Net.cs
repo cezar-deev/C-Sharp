bcbb62fcabb1399384fb2ec983de1fa87731ed8d
Cezar Cardoso <cezar.deev@gmail.com>
2021-11-01 15:06:12 -0300

    ATUALIZAR

---
+++ Exercicos/Ambiente .Net.cs
@@ -0,0 +1,7 @@
+/*
+
+1 - SDK :  O kit de Desenvolvimento de Software com diversos recursos, entre eles o compilador da linguagem C#.
+
+2 - IDE/Editor : Uma ferramenta para escrevermos códigos.Instalaremos o Visual Studio Code, um editor leve e fácil de usar.
+
+*/
---
+++ Exercicos/CLI.cs
@@ -0,0 +1,25 @@
+/*
+
+                <<   CLI (Interface de Linha de Comando)  >>
+
+CLI : Em geral, o desenvolvimento de uma aplicação passa por três estágios que são, a criação do projeto, codificação e publicação. Assim como outros frameworks modernos para desenvolvimento de aplicações, .NET Core tem uma ferramenta que é utilizada no prompt de comando ou no terminal, que auxilia em todos estes estágios.
+
+Esta ferramenta chama-se CLI (Interface de Linha de Comando), é através dela que vamos criar uma aplicação, incluir componentes, compilar o código, entre outras funcionalidades. Esta maneira de programar usando uma CLI é cada vez mais utilizada por causa de sua produtividade, porque em um mesmo comando podemos executar diversas tarefas.
+
+Por exemplo ao adicionar um Controller para uma aplicação MVC na mesma linha de código informamos sua Model, seu contexto de banco de dados e em que diretório deve ser gravado.
+
+Para entender melhor esta ferramenta, vamos acompanhar a sua utilização no Windows. Vamos abrir nosso prompt de comando e executar algumas tarefas de desenvolvimento de aplicações.
+
+Ao digitar a palavra “dotnet” seguida de um comando específico, executamos tarefas no desenvolvimento de aplicações. Por exemplo:
+
+Ao digitar “dotnet new”, criamos um novo projeto .NET Core
+Ao digitar “dotnet build”, compilamos o código de uma aplicação
+Ao digitar “dotnet run”, executamos uma aplicação.
+Além destes comandos, existem outros que conheceremos em outros vídeos desta matéria.
+
+Como a CLI é nossa principal ferramenta no desenvolvimento de aplicações, a ferramenta para escrever nossos códigos é uma escolha livre do desenvolvedor. Podemos usar um editor de códigos leve como o Visual Studio Code ou uma IDE mais robusta como o Visual Studio.
+
+Criando o projeto com a CLI :
+
+*/
+
