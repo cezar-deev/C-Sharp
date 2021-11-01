3c4653f373a971a8971d18d4f84f23e26fbf4904
Cezar Cardoso <cezar.deev@gmail.com>
2021-11-01 17:36:28 -0300

    ATUALIZAR

--- Exercicos/Poo.cs
+++ Exercicos/Poo.cs
@@ -1,14 +1,11 @@
 /* 
 
-CLASSES :
+                                                         << CLASSES >> :
 
 Uma classe é uma estrutura que abstrai um conjunto de objetos com características similares. 
 Uma classe define o comportamento de seus objetos - através de métodos - e os estados possíveis destes objetos - através de atributos.
-
 Em outras palavras, uma classe descreve os serviços oferecidos por seus objetos e quais informações eles podem armazenar.
-
 Classes não são diretamente suportadas em todas as linguagens, e são necessárias para que uma linguagem seja orientada a objetos.
-
 Uma classe representa um conjunto de objetos com características afins. 
 Uma classe define o comportamento dos objetos através de seus métodos, e quais estados ele é capaz de manter através de seus atributos.
 
@@ -39,17 +36,17 @@ nome: um identificador para o método.
      ->lista de argumentos: quando o método recebe parâmetros para sua execução, o tipo e um identificador para cada parâmetro.
      ->visibilidade: como para atributos, define o quão visível é um método a partir de objetos de outros classes.
 
-As técnicas de programação orientada a objetos recomendam que a estrutura de um objeto e a implementação de seus métodos devem ser tão privativos como possível. Normalmente, os atributos de um objeto não devem ser visíveis externamente. Da mesma forma, de um método deve ser suficiente conhecer apenas sua especificação, sem necessidade de saber detalhes de como a funcionalidade que ele executa é implementada.
+As técnicas de programação orientada a objetos recomendam que a estrutura de um objeto e a implementação de seus métodos devem ser tão privativos como possível.
+Normalmente, os atributos de um objeto não devem ser visíveis externamente. Da mesma forma, de um método deve ser suficiente conhecer apenas sua especificação,
+sem necessidade de saber detalhes de como a funcionalidade que ele executa é implementada.
 
 
-
-
-OBJETOS : 
+                                                             << OBJETOS >> : 
 
 Um objeto, na vida real, é qualquer coisa a qual pudermos dar um nome.
-
-Um objeto, em programação orientada a objetos, é uma instância (ou seja, um exemplar) de uma classe. A Wikilivros é um exemplo de Wiki, ou, a Wikilivros é uma instância de Wiki. Isto poderia ser representado em um programa orientado a objetos com uma classe chamada Wiki e um objeto do tipo Wiki chamado Wikilivros.
-
+Um objeto, em programação orientada a objetos, é uma instância (ou seja, um exemplar) de uma classe. 
+A Wikilivros é um exemplo de Wiki, ou, a Wikilivros é uma instância de Wiki. Isto poderia ser representado em um programa orientado a objetos com uma
+classe chamada Wiki e um objeto do tipo Wiki chamado Wikilivros.
 Um objeto é capaz de armazenar estados através de seus atributos e reagir a mensagens enviadas a ele, assim como se relacionar e enviar mensagens a outros objetos.
 
 
@@ -60,10 +57,23 @@ O conjunto de valores dos atributos de um determinado objeto é chamado de estad
 
 
 
-HERANÇA :
+                                                              << HERANÇA >> 
 
- É um princípio de orientação a objetos, que permite que classes compartilhem atributos e métodos, através de "heranças". Ela é usada na intenção de reaproveitar código ou comportamento generalizado ou especializar operações ou atributos. O conceito de herança de várias classes é conhecido como herança múltipla. Como exemplo pode-se observar as classes 'aluno' e 'professor', onde ambas possuem atributos como nome, endereço e telefone. Nesse caso pode-se criar uma nova classe chamada por exemplo, 'pessoa', que contenha as semelhanças entre as duas classes, fazendo com que aluno e professor herdem as características de pessoa, desta maneira pode- se dizer que aluno e professor são subclasses de pessoa. Também podemos dizer que uma classe pode ser abstrata(abstract) ou seja ela não pode ter uma instância, ela apenas "empresta" seus atributos e metódos como molde para novas classes.
+ É um princípio de orientação a objetos, que permite que classes compartilhem atributos e métodos, através de "heranças". 
+ Ela é usada na intenção de reaproveitar código ou comportamento generalizado ou especializar operações ou atributos. 
+ O conceito de herança de várias classes é conhecido como herança múltipla. Como exemplo pode-se observar as classes 'aluno' e 'professor', 
+ onde ambas possuem atributos como nome, endereço e telefone. Nesse caso pode-se criar uma nova classe chamada por exemplo, 'pessoa', 
+ que contenha as semelhanças entre as duas classes, fazendo com que aluno e professor herdem as características de pessoa, 
+ desta maneira pode- se dizer que aluno e professor são subclasses de pessoa. Também podemos dizer que uma classe pode ser abstrata(abstract) ou 
+ seja ela não pode ter uma instância, ela apenas "empresta" seus atributos e metódos como molde para novas classes.
 
+                                                             << ABSTRAÇÃO >>
+ 
+ É a habilidade de concentrar nos aspectos essenciais de um contexto qualquer, ignorando características menos importantes ou acidentais. 
+ Em modelagem orientada a objetos, uma classe é uma abstração de entidades existentes no domínio do sistema de software.
+
+Por exemplo, imaginamos a abstração referente a classe Animais. Há várias entidades na classe Animais como Anfíbios, 
+Répteis e Mamíferos que são também sub-classes da classe Animais, onde há objetos que contêm cada sub-classe como Ser-humano, Jacaré e outros
  
 */            
 
