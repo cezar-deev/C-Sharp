fd0f576d2ecc461234d513a0effa0547e30db14f
Cezar Cardoso <cezar.deev@gmail.com>
2022-03-21 21:51:00 -0300

    Atualizar

---
+++ Calculator/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Clear();
+Console.WriteLine("primeiro valor");
+float v1 = float.Parse(Console.ReadLine());
+
+Console.WriteLine(v1);
