4c8a8eb1ec187ed62f9ca34fe38e130e1560f312
Cezar Cardoso <cezar.deev@gmail.com>
2022-04-06 20:29:41 -0300

    Atualizar

---
+++ Poo/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+//--------------------------------------------------------------------------------
+// Struct - Estrutura de dados ( armazena o valor em si - Tipo por Valor )
+//--------------------------------------------------------------------------------
+/*
+static void Main (string[] args) 
+{
+    var Customer = new Customer();
+    Customer.Name = "teste";
+
+}
+
+struct Customer  
+{ 
+    public string Name;
+}
+*/
+
+//--------------------------------------------------------------------------------
+// Encapsulamento - 
+//--------------------------------------------------------------------------------
+
+/*
+class Pagamento
+{
+    // Propriedades - (são as variaveis)
+    DateTime Vencimento; 
+    
+    // Metodos - (São as Funções)
+    void Pagar() {}
+}
+*/
+
+//--------------------------------------------------------------------------------
+// Abstração - Expor somente o necessário
+//--------------------------------------------------------------------------------
+
+class Pagamento
+{
+    // Propriedades - (são as variaveis)
+    DateTime Vencimento; 
+    
+    // Metodos - (São as Funções)
+    void Pagar() {
+        ConsultarSaldodoCartao();
+    }
+    Private void ConsultarSaldodoCartao() {
+
+    }
+}
