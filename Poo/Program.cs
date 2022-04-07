2cc818f547fc0b6429a2016b4a1d713c82a55646
Cezar Cardoso <cezar.deev@gmail.com>
2022-04-07 20:36:49 -0300

    atualizar

--- Poo/Program.cs
+++ Poo/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 
 
 //--------------------------------------------------------------------------------
@@ -21,7 +21,6 @@ struct Customer
 //--------------------------------------------------------------------------------
 // Encapsulamento - 
 //--------------------------------------------------------------------------------
-
 /*
 class Pagamento
 {
@@ -37,6 +36,8 @@ class Pagamento
 // Abstração - Expor somente o necessário
 //--------------------------------------------------------------------------------
 
+/*
+
 class Pagamento
 {
     // Propriedades - (são as variaveis)
@@ -50,3 +51,131 @@ class Pagamento
 
     }
 }
+*/
+
+//--------------------------------------------------------------------------------
+// Herança - É a capacidade que um objeto tem de herdar propriedades, metodos e eventos e outro objeto.
+//--------------------------------------------------------------------------------
+
+/*
+static void Main (string[] args) 
+{
+    var pagamentoBoleto = new PagamentoBoleto(); // Instanciando um objeto da class PagamentoBoleto 
+
+    pagamentoBoleto.Vencimento = DateTime.Now; // herdando a propriedade da class Pagamento
+    pagamentoBoleto.NumeroBoleto = "1234";
+
+    pagamentoBoleto.Pagar(); // herdando o metodo Pagar() da class Pagamento
+    pagamentoBoleto.Parcelar();
+
+
+}
+
+    class Pagamento
+    {
+        // Propriedades 
+        public DateTime Vencimento; 
+    
+        // Metodos  
+        public void Pagar() {}
+
+        public void Parcelar() {}
+            
+    }
+        
+    class PagamentoBoleto : Pagamento // Aqui herdo as propriedades e metodos da class pagamento.
+    {
+        public string NumeroBoleto;
+        public string nome;
+    }
+
+    class PagamentoCartaocredito : Pagamento 
+    {
+        public string Numero;
+        public string vencimento;
+        public string codigo;
+
+    }
+
+    class PagamentoPix: Pagamento 
+        {
+            public string Numero;
+            public string vencimento;
+            
+
+    }
+    
+*/
+
+//--------------------------------------------------------------------------------
+// Polimorfismo - poli( muitos ) + morf ( fomra ) - Um metodo,uma classe pode ter diversas formas
+//--------------------------------------------------------------------------------
+
+/*
+static void Main (string[] args) 
+{
+    var pagamentoBoleto = new PagamentoBoleto(); // Instanciando um objeto da class PagamentoBoleto 
+
+    pagamentoBoleto.Vencimento = DateTime.Now; // herdando a propriedade da class Pagamento
+    pagamentoBoleto.NumeroBoleto = "1234";
+
+    pagamentoBoleto.Pagar(); // herdando o metodo Pagar() da class Pagamento
+    pagamentoBoleto.Parcelar();
+
+
+}
+
+    class Pagamento
+    {
+        // Propriedades 
+        public DateTime Vencimento; 
+    
+        // Metodos  
+        public virtual void Pagar() {} // ( virtual no Pai ) permite que este metodo seja sobre escrito
+
+        public void Parcelar() {}
+            
+    }
+        
+    class PagamentoBoleto : Pagamento // Aqui herdo as propriedades e metodos da class pagamento.
+    {
+        public string NumeroBoleto;
+        
+        public override void Pagar()  // ( override no filho) estou sobreescrevendo, estou criando uma forma nova de pagar.
+        {
+            // Descrever Regra do cartão de credito
+        }
+    }
+    
+    class PagamentoCartaocredito : Pagamento 
+    {
+
+        public string Numero;
+        
+        public override void Pagar()  // ( override no filho) estou sobreescrevendo, estou criando uma forma nova de pagar.
+        {
+            // Descrever Regra do cartão de credito
+        }
+
+    }
+
+*/
+
+//--------------------------------------------------------------------------------
+// Modificadores de acesso - Se estendem a classes, propriedades e metodos
+//--------------------------------------------------------------------------------
+
+/*
+
+// Usado em : Classes
+// Usado em : propriedades
+// Usado em : metodos
+
+// Private    :  Visivel ao escopo da sua classe;
+// protected  :  Visivel ao escopo da sua classe + as classes filhos;
+// internal   :  Visivel dentro do name space ( quase não se usa );
+// public     :  Visivel esta vissivel em qualquer lugar;
+
+// Ao nao colocar nada ela estara privada
+
+*/
